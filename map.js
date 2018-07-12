ddd2e907289a00de861efac8c890d4c449974c42
gai1219 <gaishi1219@gmail.com>
2018-07-12 17:38:49 +0900

    1

--- map.js
+++ map.js
@@ -1,13 +1,4 @@
 var map;
-var latestdata;
-var gotdata;
-var i;
-var datacount;
-
-function setDatabase(datacount) {
-    eval("data" + datacount + " = " + latestdata + ";");
-    datacount++;
-}
 
 function onFirstOpenMap() { //最初に地図が表示されたときの処理
     var opts = {
@@ -17,42 +8,18 @@ function onFirstOpenMap() { //最初に地図が表示されたときの処理
     map = new google.maps.Map(document.getElementById("map"), opts);
 }
 
-function getJson() {
-    //Json取得する処理
-    $.ajax({
-       url: "https://gai1219.github.io/data.json",
-       datatype : 'jsonp',
-    }).done(function(data) {
-        if(i == 0) {
-            console.log("最初のデータを取得");
-            latestdata = data;
-            i = data.dataid;
-            setMarker();
-            setDatabase(datacount);
-        } else {
-            if(data.dataid - 1 == i) {
-                latestdata = data;
-                i = data.dataid;
-                setDatabase(datacount);
-                console.log("data更新成功！" + i);
-                setMarker();
-            } else {
-                console.log("dataidがおかしい");
-            }
-        }
-    })
+function onClick() {
+    //phpでdbからデータをもらう処理
 }
 
 function setMarker() {
-    var latlng = new google.maps.LatLng(latestdata.ido, latestdata.keido);
-//    var marker = new google.maps.Marker({
-//        position: latlng,
-//        title: "YAY"
-//    });
-    eval("marker" + datacount + " = new google.maps.Marker({position: " + latlng + "});");
-//    marker.setMap(map);
-    eval("marker" + datacount + ".setMap(" + map + ");");
+    var marker = new google.maps.Marker();
+    var markerObj = new Object;
+    markerObj.lat = //phpの緯度変数
+    markerObj.lng = //phpの経度変数
+    markerObj.kion = //気温
+    //...オブジェクトと変数の設定
+    
 }
+ 
 
-//        "ido":"35.710063",     
-//        "keido":"139.8107"
