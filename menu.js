da3b35374b9f7bd8fe4c59d4973104cb75b94c6a
gai1219 <gaishi1219@gmail.com>
2018-07-16 18:51:53 +0900

    1

--- menu.js
+++ menu.js
@@ -1,16 +1,30 @@
+//ページ読み込み時
 $(document).ready( function(){
     $("#close").hide();
     $(".menu").css("margin-left", "-190px");
+    $("#all").hide();
 });
 
+//メニュー開いたとき
 $("#open").click( function() {
     $("#open").hide();
     $("#close").show();
     $(".menu").animate({"margin-left":'0px'},300);
+    $("#all").show();
 });
 
+//メニュー閉じたとき
 $("#close").click( function() {
     $("#open").show();
     $("#close").hide();
     $(".menu").animate({"margin-left":'-190px'},300);
+});
+
+//メニュー表示時にメニュー外クリックで閉じる
+$("#main").click( function() {
+    if(!$('#open').is(':visible')) {
+        $("#open").show();
+        $("#close").hide();
+        $(".menu").animate({"margin-left":'-190px'},300);
+    }
 });
