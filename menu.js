4b1c07f762e7db0941d0c7d1c02db648ff329c2b
gai1219 <gaishi1219@gmail.com>
2018-07-16 18:01:29 +0900

    1

--- menu.js
+++ menu.js
@@ -1,3 +1,16 @@
-//var showhide;
-//$("#button").toggle(showhide);
-//
+$(document).ready( function(){
+    $("#close").hide();
+    $(".menu").css("margin-left", "-190px");
+});
+
+$("#open").click( function() {
+    $("#open").hide();
+    $("#close").show();
+    $(".menu").animate({"margin-left":'0px'},300);
+});
+
+$("#close").click( function() {
+    $("#open").show();
+    $("#close").hide();
+    $(".menu").animate({"margin-left":'-190px'},300);
+});
