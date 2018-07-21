256f14e8fe034cbf07a69b886af909a978ea443a
gai1219 <gaishi1219@gmail.com>
2018-07-21 16:38:40 +0900

    1

--- data.js
+++ data.js
@@ -1,72 +1,63 @@
-var firstdata = [];
+var latestdata = [];
 var data = [];
 
-function firstOpen() {
+function firstOpen() { //ページオープン時
     $.ajax({
            url: "https://sheets.googleapis.com/v4/spreadsheets/185yPvhNOnMogOv-C3Xj0PC0r_4JazkcRrp6B7XHQ6q0/values/%E3%82%B7%E3%83%BC%E3%83%881!A3:F6?key=AIzaSyDiSaDpQ61kQbjP5SfGhdiFfmloJrJvjEE",
            datatype : 'jsonp',
-       }).done(function(data) { //成功
-            var firstdatacount = 0;
-            for(var x = 0; x < data.values.length; x++) {
-                if(data.values[x].length == 6) {
-                    firstdata[firstdatacount] = data.values[x];
-                    firstdatacount++;
+       }).done(function(response) { //成功
+            var latestdatacount = 0;
+            for(var x = 0; x < response.values.length; x++) {
+                if(response.values[x].length == 6) {
+                    latestdata[latestdatacount] = response.values[x];
+                    latestdatacount++;
                 } else {
                     //異常なデータ スルー
                 }
             }
             onOpenMap();
-            setFirstData();
-       }).fail(function(data) { //失敗
+            setFirstMarker();
+       }).fail(function(response) { //失敗
             showAlert("データ取得に失敗しました。");
        });
 }
 
-function setFirstData() {
-    data = [];
-    var firstdatacount = firstdata.length;
-    var marker = null;
-    
-    for(var x = 0; x < firstdata.length; x++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(parseFloat(firstdata[x][4]), parseFloat(firstdata[x][5])),
-            map: map,
-            icon: new google.maps.MarkerImage (
-                'point.png'
-            ),
-        });
-        
-        data[firstdatacount - x] = [];
-        
-        data[firstdatacount - x][0] = marker;
-        data[firstdatacount - x][1] = firstdata[x][0];
-        data[firstdatacount - x][2] = firstdata[x][1];
-        data[firstdatacount - x][3] = firstdata[x][2];
-        data[firstdatacount - x][4] = firstdata[x][3];
-        data[firstdatacount - x][5] = firstdata[x][4];
-        data[firstdatacount - x][6] = firstdata[x][5];
-        
-        setClickEvent(firstdatacount - x);
-        
-    }
-    
+function getData() { //10秒毎
+    var getdata = [];
+    $.ajax({
+           url: "https://sheets.googleapis.com/v4/spreadsheets/185yPvhNOnMogOv-C3Xj0PC0r_4JazkcRrp6B7XHQ6q0/values/%E3%82%B7%E3%83%BC%E3%83%881!A3:F6?key=AIzaSyDiSaDpQ61kQbjP5SfGhdiFfmloJrJvjEE",
+           datatype : 'jsonp',
+       }).done(function(response) { //成功
+            if(response.values.toString() == latestdata.toString()) {
+                //更新データなし
+            } else {
+                //更新データあり
+                showAlert("更新!");
+                console.log("更新!");
+                
+                map.panTo(new google.maps.LatLng(response.values[0][4], response.values[0][5]));
+                
+                latestdata = response.values;
+                
+                setMarker(response.values);
+            }
+       }).fail(function(response) { //失敗
+            showAlert("データ取得に失敗しました。");
+       });
 }
 
-function setClickEvent(x) {
-    data[x][0].addListener('click', function() { // マーカーをクリックしたとき
-        $("#エアロゾル1 nobr").text(data[x][1]);
-        $("#エアロゾル2 nobr").text(data[x][2]);
-        $("#気温 nobr").text(data[x][3]);
-        $("#気圧 nobr").text(data[x][4]);
-        $("#緯度 nobr").text(data[x][5]);
-        $("#経度 nobr").text(data[x][6]);
+function autoReload() {
+    var count = 0;
+    var countup = function(){
+        console.log("count++");
+        count++;
         
-        $(".datawindow").show();
-    });
-}
-
-function setData() {
-    
+        if(count % 10 == 0) {
+            console.log("10秒");
+            getData();
+        }
+    } 
+    setInterval(countup, 1000); //10秒毎
 }
 
 
--- map.js
+++ map.js
@@ -5,11 +5,11 @@ function onFirstOpenMap() { //最初に地図が表示されたときの処理
 }
 
 function onOpenMap() { //最初に地図が表示されたときの処理
-//    var lat = parseFloat(firstdata[0][4]);
-//    var lng = parseFloat(firstdata[0][5]);
+    var lat = parseFloat(latestdata[0][4]);
+    var lng = parseFloat(latestdata[0][5]);
     var options = {
         zoom: 18,
-        center: new google.maps.LatLng(35.6605, 139.729056),
+        center: new google.maps.LatLng(lat, lng),
         streetViewControl: false,
         fullscreenControl: false,
         mapTypeControlOptions: { 
@@ -18,4 +18,85 @@ function onOpenMap() { //最初に地図が表示されたときの処理
     };
     map = new google.maps.Map(document.getElementById("map"), options);
     showAlert("地図を表示しました。");
+}
+
+function setFirstMarker() {
+    data = [];
+    var latestdatacount = latestdata.length;
+    var marker = null;
+    
+    for(var x = 0; x < latestdata.length; x++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(parseFloat(latestdata[x][4]), parseFloat(latestdata[x][5])),
+            map: map,
+            icon: new google.maps.MarkerImage (
+                'point.png'
+            ),
+        });
+        
+        data[latestdatacount - x] = [];
+        
+        data[latestdatacount - x][0] = marker;
+        data[latestdatacount - x][1] = latestdata[x][0];
+        data[latestdatacount - x][2] = latestdata[x][1];
+        data[latestdatacount - x][3] = latestdata[x][2];
+        data[latestdatacount - x][4] = latestdata[x][3];
+        data[latestdatacount - x][5] = latestdata[x][4];
+        data[latestdatacount - x][6] = latestdata[x][5];
+        
+        setFirstMarkerClick(latestdatacount - x);
+        
+    }
+    
+    autoReload();
+}
+
+function setFirstMarkerClick(x) {
+    data[x][0].addListener('click', function() { // マーカーをクリックしたとき
+        $("#エアロゾル1 nobr").text(data[x][1]);
+        $("#エアロゾル2 nobr").text(data[x][2]);
+        $("#気温 nobr").text(data[x][3]);
+        $("#気圧 nobr").text(data[x][4]);
+        $("#緯度 nobr").text(data[x][5]);
+        $("#経度 nobr").text(data[x][6]);
+        
+        $(".datawindow").show();
+    });
+}
+
+function setMarker(response) {
+    marker = new google.maps.Marker({ //マーカー作成
+        position: new google.maps.LatLng(response[0][4], response[0][5]),
+        map: map,
+        icon: new google.maps.MarkerImage(
+            "point.png"
+        ),
+    });
+    
+    var addarray = [];
+    
+    addarray[0] = marker;
+    addarray[1] = response[0][0];
+    addarray[2] = response[0][1];
+    addarray[3] = response[0][2];
+    addarray[4] = response[0][3];
+    addarray[5] = response[0][4];
+    addarray[6] = response[0][5];
+    
+    data.push(addarray);
+    
+    setMarkerClick();
+}
+
+function setMarkerClick() {
+    data[data.length - 1][0].addListener('click', function() { // マーカーをクリックしたとき
+        $("#エアロゾル1 nobr").text(data[data.length - 1][1]);
+        $("#エアロゾル2 nobr").text(data[data.length - 1][2]);
+        $("#気温 nobr").text(data[data.length - 1][3]);
+        $("#気圧 nobr").text(data[data.length - 1][4]);
+        $("#緯度 nobr").text(data[data.length - 1][5]);
+        $("#経度 nobr").text(data[data.length - 1][6]);
+        
+        $(".datawindow").show();
+    });
 }
